3f4122f7e48c55237b41329ba1bf821709a62fdd
tvkvds <tvkvds@gmail.com>
2021-07-27 13:52:12 +0200

    query function + user changed to show pages

---
+++ views/skills/show.view.php
@@ -0,0 +1,20 @@
+<?php require 'views/partials/header.view.php' ?>
+
+<!--
+            ['skill' => 'HTML', 'description' => '', 'category' => 'Development', 'in_progress' => '0'],
+-->     
+
+<div class="main">
+
+
+  
+<div>
+<?php $skills = $vars->skills; ?>
+  <ul>
+  <?php foreach ($skills as $skill) : ?>
+    <li><?= 'Skill: ' . $skill['skill'] . ' - ' . $skill['category'] . ' - ' . $skill['in_progress'];?></li>
+    <?php endforeach; ?>
+  </ul>
+</div>
+
+<?php require 'views/partials/footer.view.php' ?>
